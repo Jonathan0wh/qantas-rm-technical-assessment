68d4e5c8b4381506febea3790f91fc18992e0d57
Jonathan <Jonathan0wh@users.noreply.github.com>
2022-01-30 01:21:45 +1100

    fix: upgrade npm deps

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+
+import "./App.css";
 
 const App: React.FC = () => {
   return <div>Your app</div>;
-}
+};
 
 export default App;
